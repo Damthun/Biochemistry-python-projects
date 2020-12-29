3fb861af15a69bea59cf6fbc8768389338f42a51
Dametre Thunberg <40583179+Damthun@users.noreply.github.com>
2020-12-29 17:19:29 -0600

    Update lab one part two.py

--- lab one part two.py
+++ lab one part two.py
@@ -1,16 +1,11 @@
-##Copy your program from the first exercise, and add the following functionality:
-#Display the original sequence for the user at the start of the session, all in uppercase.
-#Allow the user to specify where the substitution will start and end.
-#Allow the user to specify the sequence that will be substituted.
-#Display the altered sequence using the same uppercase/lowercase convention from the first exercise
-
-seQ = "aatcgcgattagtcatgaaaat"
-seQ2 = seQ.upper()
+# This exercise takes the functionality of the first exercise, but adds the ability to choose what section of the sequence to replace with a new user-defined section.
+seQ = "aatcgcgattagtcatgaaaat" # initial sequence
+seQ2 = seQ.upper() #making the sequence uppercase, Just a preference for me.
 
 print("The current sequence is :"+ seQ2)
-start = input('What value does your change start at? (note that indexing starts at zero)')
-end = input('What value does your change end at?')
-replacement = input("what is the new sequence you'd like to be input?")
-newSeq = seQ2.replace(seQ2[int(start):int(end)], replacement.lower())
+start = input('What value does your change start at? (note that indexing starts at zero)') # prompts the user to pick where they'd like to start their splice.
+end = input('What value does your change end at?') #prompts the user to pick an end point for their splice.
+replacement = input("what is the new sequence you'd like to be input?") #prompts the user to pick their insert.
+newSeq = seQ2.replace(seQ2[int(start):int(end)], replacement.lower()) #creates a new variable that stores the new sequence by replacing the user given range with the given insert.
 
 print(newSeq)
